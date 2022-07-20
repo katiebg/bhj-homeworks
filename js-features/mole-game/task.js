2bec9cc6282ec08de5236b421eb3d10f6ccdba40
katiebg <97286492+katiebg@users.noreply.github.com>
2022-07-20 11:05:35 +0300

    Add files via upload

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,10 +1,10 @@
-let holes = [document.getElementById("hole1"), document.getElementById("hole2"), document.getElementById("hole3"), document.getElementById("hole4"),
+const holes = [document.getElementById("hole1"), document.getElementById("hole2"), document.getElementById("hole3"), document.getElementById("hole4"),
 document.getElementById("hole5"),document.getElementById("hole6"), document.getElementById("hole7"), document.getElementById("hole8"), document.getElementById("hole9")];
 let dead = document.getElementById("dead");
 let lost = document.getElementById("lost");
-for (i = 0; i < 9; i++) {
+for (let i = 0; i < 9; i++) {
     holes[i].onclick = function checkHole() {
-        if (holes[i].classList.contains( 'hole_has-mole' )) {
+        if (holes[i].className.includes( 'hole_has-mole' )) {
             dead.textContent++;
             if (dead.textContent == 10) {
                 alert("Вы выиграли!");
