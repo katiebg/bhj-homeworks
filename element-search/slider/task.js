e803192155aa42467e75890861fe234c79e4bcce
katiebg <97286492+katiebg@users.noreply.github.com>
2022-08-01 17:56:40 +0300

    Add files via upload

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,41 +1,25 @@
 const arrowRight = document.getElementsByClassName("slider__arrow_next");
 const arrowLeft = document.getElementsByClassName("slider__arrow_prev");
-
-
 let slides = Array.from(document.querySelectorAll("div.slider__items > div"));
-let activeSlide = slides[0];
-arrowRight.onclick = slideNext;
 
-function slideNext() {
-    for (let i = 0; i < 5; i++) {
-        if (slides[i] === activeSlide) {
-            if (i === 4) {
-                slides[i].classList.remove("slider__item_active");
-                activeSlide = slides[0];
-                slides[0].classList.add("slider__item_active");
-            } else {
-                slides[i].classList.remove("slider__item_active");
-                activeSlide = slides[i + 1];
-                slides[i + 1].classList.add("slider__item_active");
-            }
+function changeSlide (side) {
+    let idxActiveSlide = slides.findIndex((element,idx ) => element.classList.contains("slider__item_active"));
+    slides[idxActiveSlide].classList.remove("slider__item_active");
+    if (side === "left") {
+        if (idxActiveSlide === 0) {
+            slides[slides.length - 1].classList.add("slider__item_active");
+        } else {
+            slides[idxActiveSlide - 1].classList.add("slider__item_active");
+        }
+    }
+    if (side === "right") {
+        if (idxActiveSlide === (slides.length - 1)) {
+            slides[0].classList.add("slider__item_active");
+        } else {
+            slides[idxActiveSlide + 1].classList.add("slider__item_active");
         }
     }
 }
 
-function slidePrev() {
-    for (let i = 0; i < 5; i++) {
-        if (slides[i] === activeSlide) {
-            if (i === 0) {
-                slides[i].classList.remove("slider__item_active");
-                activeSlide = slides[4];
-                slides[4].classList.add("slider__item_active");
-            } else {
-                slides[i].classList.remove("slider__item_active");
-                activeSlide = slides[i - 1];
-                slides[i - 1].classList.add("slider__item_active");
-            }
-        }
-    }
-}
-
-
+arrowLeft.onclick = changeSlide("left");
+arrowRight.onclick = changeSlide("right");
