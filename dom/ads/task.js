3769519a814df62322f5341d0f76819b90d46100
katiebg <97286492+katiebg@users.noreply.github.com>
2022-08-18 22:37:20 +0300

    Add files via upload

---
+++ dom/ads/task.js
@@ -0,0 +1,20 @@
+let rotatorCase = document.querySelectorAll(".rotator__case");
+let activeCase = document.querySelector(".rotator__case_active");
+let delay = 1000;
+
+let timerId = setTimeout(function request() {
+    if (activeCase.nextElementSibling) {
+        activeCase.classList.remove("rotator__case_active");
+        activeCase = activeCase.nextElementSibling;
+        activeCase.classList.add("rotator__case_active");
+    } else {
+        activeCase.classList.remove("rotator__case_active");
+        activeCase = rotatorCase.item(0);
+        activeCase.classList.add("rotator__case_active");
+    }
+    activeCase.style.color = activeCase.getAttribute('dataColor');
+    delay = activeCase.getAttribute('dataSpeed');
+
+    timerId = setTimeout(request, delay );
+  
+  }, delay );
