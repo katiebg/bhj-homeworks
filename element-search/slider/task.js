371a9f057cb89693f942b51de59d0ed4beb3e9f4
katiebg <97286492+katiebg@users.noreply.github.com>
2022-08-18 14:04:53 +0300

    Add files via upload

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,27 +1,26 @@
-const arrowRight = document.getElementsByClassName("slider__arrow_next");
-const arrowLeft = document.getElementsByClassName("slider__arrow_prev");
+let arrowRight = [...document.getElementsByClassName('slider__arrow slider__arrow_next')][0];
+let arrowLeft = [...document.getElementsByClassName('slider__arrow slider__arrow_prev')][0];
 let slides = Array.from(document.querySelectorAll("div.slider__items > div"));
 
-function changeSlide(side) {
+function changeSlideleft () {
     let idxActiveSlide = slides.findIndex((element, idx) => element.classList.contains("slider__item_active"));
     slides[idxActiveSlide].classList.remove("slider__item_active");
-    if (side === "left") {
-        if (idxActiveSlide === 0) {
-            slides[slides.length - 1].classList.add("slider__item_active");
-        } else {
-            slides[idxActiveSlide - 1].classList.add("slider__item_active");
-        }
+    if (idxActiveSlide === 0) {
+        slides[slides.length - 1].classList.add("slider__item_active");
+    } else {
+        slides[idxActiveSlide - 1].classList.add("slider__item_active");
     }
-    if (side === "right") {
-        if (idxActiveSlide === (slides.length - 1)) {
-            slides[0].classList.add("slider__item_active");
-        } else {
-            slides[idxActiveSlide + 1].classList.add("slider__item_active");
-        }
+}
+function changeSlideRight () {
+    let idxActiveSlide = slides.findIndex((element, idx) => element.classList.contains("slider__item_active"));
+    slides[idxActiveSlide].classList.remove("slider__item_active");
+    if (idxActiveSlide === (slides.length - 1)) {
+        slides[0].classList.add("slider__item_active");
+    } else {
+        slides[idxActiveSlide + 1].classList.add("slider__item_active");
     }
 }
 
-let resultLeft = () => { return changeSlide("left"); }
-let resultRight = () => { changeSlide("right"); }
-arrowLeft.onclick = resultLeft;
-arrowRight.onclick = resultRight;
+arrowRight.addEventListener('click', changeSlideRight);
+
+arrowLeft.addEventListener('click', changeSlideleft)
