a442852115a9885dd58fb2c3c6634954b484ac3e
katiebg <97286492+katiebg@users.noreply.github.com>
2022-09-06 20:51:17 +0300

    Add files via upload

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -7,12 +7,14 @@ for (let i = 0; i < menuItems.length; ++i) {
     menuItems[i].onclick = (event) => {
         subMenuElements[i] = Array.from(menuItems[i].querySelectorAll("ul.menu_sub > li"));
         if (subMenuElements[i].length > 0) {
-            event.preventDefault();
             menuSub = menuItems[i].getElementsByClassName("menu_sub");
+            if (!event.target.getAttribute('href')) {
+                event.preventDefault();
+            }           
             if (menuSub.item(0).classList.contains("menu_active")){
-                menuSub.item(0).classList.remove("menu_active");
+                menuSub.item(0).classList.toggle("menu_active");
             } else {
-                menuSub.item(0).classList.add("menu_active");
+                menuSub.item(0).classList.toggle("menu_active");
             }
         }
     }
