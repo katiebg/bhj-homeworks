e0f88e75dde4404de36769d8684f383e3160ac77
katiebg <97286492+katiebg@users.noreply.github.com>
2022-10-13 19:18:45 +0300

    Add files via upload

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -6,6 +6,9 @@ const chatInput = document.getElementById("chat-widget__input");
 let textMessage = "";
 let messageText, messageTime, messageClient;
 let date = new Date();
+let i = 0;
+const robotMessages = ["Я не знаю", "Вы меня смущаете", "Обратитесь к психиатру", "позвоните по номеру +7 (495) 570-25-11", "Вы не правы в этом вопросе",
+"Я ничем не могу помочь", "Как дела?", "Что делаешь?"]
 
 chatInput.addEventListener("keyup", (e) => {
     if (e.key == "Enter") {
@@ -20,11 +23,22 @@ chatInput.addEventListener("keyup", (e) => {
         </div>
         `;
         messageClient = messages.querySelectorAll(".message_client");
-        messageText = messageClient.item(messageClient.lenght - 1).querySelector(".message__text");
+        messageText = messageClient.item(messageClient.length - 1).querySelector(".message__text");
         messageText.textContent = textMessage;
-        messageTime = messageClient.item(messageClient.lenght - 1).querySelector(".message__time");
+        messageTime = messageClient.item(messageClient.length - 1).querySelector(".message__time");
         messageTime.textContent = date.getHours() + ':' + date.getMinutes();
+        chatInput.value = ' ';
+        messages.innerHTML += `<div class="message">
+        <div class="message__time">22:10</div>
+        <div class="message__text">Добрый день!</div>
+        </div>
+        `
+        messageRobot = messages.querySelectorAll(".message");
+        messageTextRob = messageRobot.item(messageRobot.length - 1).querySelector(".message__text")
+        messageTextRob.textContent = robotMessages[i%robotMessages.length];
+        i++;
         } 
+
     }
 })
 
