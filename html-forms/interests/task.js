5dee17797d3f14bc441dc52e9e67e08e6f2a6324
katiebg <97286492+katiebg@users.noreply.github.com>
2022-10-18 22:44:14 +0300

    Add files via upload

--- html-forms/interests/task.js
+++ html-forms/interests/task.js
@@ -1,30 +1,17 @@
-const side = document.querySelector(".chat-widget__side");
-const chatWidjet = document.querySelector(".chat-widget");
-side.addEventListener("click", () => chatWidjet.classList.add("chat-widget_active"));
-const messages = document.querySelector( '.chat-widget__messages' );
-const chatInput = document.getElementById("chat-widget__input");
-let textMessage = "";
-let messageText, messageTime, messageClient;
-let date = new Date();
+const interest = document.querySelectorAll('input');
+let child, checkboxButton;
 
-chatInput.addEventListener("keyup", (e) => {
-    if (e.key == "Enter") {
-        textMessage = chatInput.value;
-        if (textMessage != ""){
-        messages.innerHTML += `
-        <div class="message message_client">
-            <div class="message__time">09:21</div>
-            <div class="message__text">
-            Добрый день, мы ещё не проснулись. Позвоните через 10 лет
-            </div>
-        </div>
-        `;
-        messageClient = messages.querySelectorAll(".message_client");
-        messageText = messageClient.item(messageClient.lenght - 1).querySelector(".message__text");
-        messageText.textContent = textMessage;
-        messageTime = messageClient.item(messageClient.lenght - 1).querySelector(".message__time");
-        messageTime.textContent = date.getHours() + ':' + date.getMinutes();
-        } 
+interest.forEach((element) => element.addEventListener("change", () => 
+{
+    let container = element.parentElement.parentElement.querySelectorAll("ul");
+    child = container.item(0).children
+    for (let i = 0; i<child.length; i++){
+        checkboxButton = child.item(i).children;
+        if (element.checked === true){
+            checkboxButton.item(0).querySelector("input").checked = true;
+        } else {
+            checkboxButton.item(0).querySelector("input").checked = false;
+        }
     }
-})
-
+}
+))
