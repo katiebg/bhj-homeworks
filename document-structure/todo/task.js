55bdb3ce8cc7f2e70d544c556ec388a7e616433b
katiebg <97286492+katiebg@users.noreply.github.com>
2023-01-11 14:23:25 +0300

    Add files via upload

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,31 +1,23 @@
 const tasksInput = document.querySelector(".tasks__input")
 const tasksList = document.querySelector(".tasks__list");
 const addtask = document.querySelector(".tasks__add");
-let dataId = 1;
 addtask.addEventListener("click", (e) => {
     e.preventDefault();
-})
-tasksInput.addEventListener("keyup", (e) => {
-    e.preventDefault();
-    if (e.key === "Enter") {
-        textTask = tasksInput.value;
-        e.preventDefault();
-        if (textTask != "") {
-            tasksList.innerHTML += `
+    textTask = tasksInput.value;
+    if (textTask.trim() !== "") {
+        tasksList.insertAdjacentHTML ("afterbegin", `
                 <div class="task">
                     <div class="task__title">
                         ${textTask}
                     </div>
                     <a href="#" class="task__remove">&times;</a>
-                </div>
-            `;
-            tasksInput.value = "";
-        }
+                </div>`);
+        tasksInput.value = "";
     }
     tasksList.querySelectorAll(".task").forEach((item) => {
-            item.querySelector(".task__remove").addEventListener("click", () => {
-                item.remove();
-            })
+        item.querySelector(".task__remove").addEventListener("click", () => {
+            item.remove();
+        })
     })
 })
 
